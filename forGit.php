00f52b092951c3632d25e37a151c00a333c448d6
Glitch21 <48030889+Glitch21@users.noreply.github.com>
2019-03-04 23:55:35 +0300

    create PHP file

---
+++ forGit.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>git problem</title>
+	<link rel="stylesheet" href="style.css">
+
+</head>
+<body style="background-color:#333">
+		<?php
+$m = 23;
+$q = 44;
+echo $m + $q;
+?>
+
+</body>
+</html>
